41e098e1befa9ae97f9a762fd4565806971c3916
TarekIdrees <tarekidrees5@gmail.com>
2024-03-06 13:07:13 +0100

    add needed configuration

--- JetFit/app/src/main/java/com/google/jetfit/presentation/theme/Color.kt
+++ JetFit/app/src/main/java/com/google/jetfit/presentation/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val onSurface = Color(0xFFC4C7C4)
+val onSurfaceVariant = Color(0xFFBFC9C3)
+val background = Color(0xFF191C1B)
+val surfaceVariant = Color(0xFF3F4945)
+val inverseOnSurface = Color(0xFF191C1B)
--- JetFit/app/src/main/java/com/google/jetfit/presentation/theme/Type.kt
+++ JetFit/app/src/main/java/com/google/jetfit/presentation/theme/Type.kt
@@ -1,36 +1,142 @@
 package com.google.jetfit.presentation.theme
 
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextMotion
 import androidx.compose.ui.unit.sp
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.Typography
+import com.google.jetfit.R
+
+private val Ibm = FontFamily(
+    fonts = listOf(
+        Font(R.font.ibm_plex_sans_regular, weight = FontWeight.Normal)
+    )
+)
 
 // Set of Material typography styles to start with
 @OptIn(ExperimentalTvMaterial3Api::class)
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        letterSpacing = (-0.25).sp,
+        fontFamily = Ibm,
+        textMotion = TextMotion.Animated
+    ),
+    displayMedium = TextStyle(
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp,
+        fontFamily = Ibm,
+        textMotion = TextMotion.Animated
+    ),
+    displaySmall = TextStyle(
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp,
+        fontFamily = Ibm,
+        textMotion = TextMotion.Animated
+    ),
+    headlineLarge = TextStyle(
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp,
+        fontFamily = Ibm,
+        textMotion = TextMotion.Animated
+    ),
+    headlineMedium = TextStyle(
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp,
+        fontFamily = Ibm,
+        textMotion = TextMotion.Animated
+    ),
+    headlineSmall = TextStyle(
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp,
+        fontFamily = Ibm,
+        textMotion = TextMotion.Animated
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp,
+        fontFamily = Ibm,
+        textMotion = TextMotion.Animated
+    ),
+    titleMedium = TextStyle(
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.15.sp,
+        fontFamily = Ibm,
+        textMotion = TextMotion.Animated
+    ),
+    titleSmall = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.1.sp,
+        fontFamily = Ibm,
+        textMotion = TextMotion.Animated
+    ),
+    labelLarge = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.1.sp,
+        fontFamily = Ibm,
+        textMotion = TextMotion.Animated
+    ),
+    labelMedium = TextStyle(
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.25.sp,
+        fontFamily = Ibm,
+        textMotion = TextMotion.Animated
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.1.sp,
+        fontFamily = Ibm,
+        textMotion = TextMotion.Animated
+    ),
+    bodyLarge = TextStyle(
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.5.sp,
+        fontFamily = Ibm,
+        textMotion = TextMotion.Animated
+    ),
+    bodyMedium = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.25.sp,
+        fontFamily = Ibm,
+        textMotion = TextMotion.Animated
+    ),
+    bodySmall = TextStyle(
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.2.sp,
+        fontFamily = Ibm,
+        textMotion = TextMotion.Animated
     )
-    */
 )
