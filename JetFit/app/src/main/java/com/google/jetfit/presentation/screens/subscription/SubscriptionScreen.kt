d97794dac65e3f91e802a586f74a09b5dde4eab6
TarekIdrees <tarekidrees5@gmail.com>
2024-03-14 13:28:24 +0100

    extract string to string file resource

--- JetFit/app/src/main/java/com/google/jetfit/presentation/screens/subscription/SubscriptionScreen.kt
+++ JetFit/app/src/main/java/com/google/jetfit/presentation/screens/subscription/SubscriptionScreen.kt
@@ -1,9 +1,12 @@
 package com.google.jetfit.presentation.screens.subscription
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -13,8 +16,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.jetfit.R
@@ -35,32 +40,37 @@ private fun SubscriptionContent() {
         modifier = Modifier
             .fillMaxSize(),
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(28.dp),
+        horizontalArrangement = Arrangement.Center,
     ) {
         Image(
             modifier = Modifier
                 .size(width = 324.dp, height = 452.dp)
+                .border(
+                    border = BorderStroke(1.dp, Color.White.copy(0.07f)),
+                    shape = RoundedCornerShape(16.dp)
+                )
                 .clip(RoundedCornerShape(16.dp)),
             painter = painterResource(id = R.drawable.training),
             contentDescription = "training photo",
             contentScale = ContentScale.Crop
         )
+        Spacer(modifier = Modifier.width(28.dp))
         Column(
             modifier = Modifier.padding(horizontal = 16.dp),
             verticalArrangement = Arrangement.spacedBy(32.dp),
         ) {
             SubscriptionHeadline()
             SubscriptionOptions()
-            Row (
+            Row(
                 horizontalArrangement = Arrangement.spacedBy(12.dp)
-            ){
+            ) {
                 JFFilledButton(
                     modifier = Modifier.width(184.dp),
-                    buttonText = "Subscribe now",
+                    buttonText = stringResource(R.string.subscribe_now),
                 )
                 JFOutlineButton(
                     modifier = Modifier.width(184.dp),
-                    buttonText = "Restore purchases"
+                    buttonText = stringResource(R.string.restore_purchases)
                 )
             }
         }
