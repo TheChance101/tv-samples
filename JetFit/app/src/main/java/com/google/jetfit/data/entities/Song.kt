d594c41dc7df43b55f5472e1bb2c38719fcdd029
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2024-03-16 14:37:25 +0200

    add Song entity

---
+++ JetFit/app/src/main/java/com/google/jetfit/data/entities/Song.kt
@@ -0,0 +1,9 @@
+package com.google.jetfit.data.entities
+
+data class Song(
+    val id: String,
+    val title: String,
+    val audioUrl: String,
+    val author: String,
+    val createdDate: String,
+)
--- JetFit/app/src/main/java/com/google/jetfit/presentation/screens/player/audio/AudioPlayerScreen.kt
+++ JetFit/app/src/main/java/com/google/jetfit/presentation/screens/player/audio/AudioPlayerScreen.kt
@@ -1,6 +1,13 @@
 package com.google.jetfit.presentation.screens.player.audio
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -8,12 +15,19 @@ import com.google.jetfit.presentation.theme.JetFitTheme
 
 @Composable
 fun AudioPlayerScreen(viewModel: AudioPlayerViewModel = hiltViewModel()) {
+    val state by viewModel.state.collectAsState()
     AudioPlayerContent()
 }
 
 @Composable
 private fun AudioPlayerContent() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
+    }
 }
 
 
