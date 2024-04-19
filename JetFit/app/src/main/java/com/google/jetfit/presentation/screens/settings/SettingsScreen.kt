187e88b4bb3cdad1a09c7eeebd08761ad22aabab
AndrewAboalhana <hana_hany6@yahoo.com>
2024-04-19 21:13:04 +0200

    add sub setting items

--- JetFit/app/src/main/java/com/google/jetfit/presentation/screens/settings/SettingsScreen.kt
+++ JetFit/app/src/main/java/com/google/jetfit/presentation/screens/settings/SettingsScreen.kt
@@ -1,21 +1,31 @@
 package com.google.jetfit.presentation.screens.settings
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -28,6 +38,7 @@ import androidx.tv.material3.ListItemDefaults
 import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.Text
 import com.google.jetfit.R
+import com.google.jetfit.presentation.theme.surface
 import com.google.jetfit.presentation.theme.surfaceContainerHigh
 
 @Composable
@@ -36,53 +47,73 @@ fun SettingsScreen(
 ) {
     val state = viewModel.settingsUIState.collectAsState()
 
-    SettingsContent(state = state.value)
+    val selectedItem = remember { mutableStateOf<SettingsItemUIState?>(null) }
 
+    SettingsContent(
+        state = state.value,
+        selectedItem = selectedItem,
+        updateSetting = viewModel::updateSetting
+    )
 }
 
 @OptIn(ExperimentalTvMaterial3Api::class)
 @Composable
 private fun SettingsContent(
-    state: SettingsUIState
+    state: SettingsUIState,
+    selectedItem: MutableState<SettingsItemUIState?>,
+    updateSetting: (SettingsItemUIState) -> Unit = {}
 ) {
-    Column(
-        Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background),
-        horizontalAlignment = Alignment.Start
-    ) {
-        Text(
-            text = stringResource(id = R.string.settings),
-            style = MaterialTheme.typography.titleLarge,
-            color = MaterialTheme.colorScheme.onSurface,
-            modifier = Modifier.padding(top = 64.dp, start = 32.dp)
-        )
+    Row(Modifier.fillMaxSize()) {
+        Column(
+            Modifier
+                .weight(1f)
+                .background(MaterialTheme.colorScheme.background),
+            horizontalAlignment = Alignment.Start
+        ){
+            Text(
+                text = stringResource(id = R.string.settings),
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.padding(top = 64.dp, start = 32.dp)
+            )
 
-        TvLazyColumn(
-            modifier = Modifier.fillMaxWidth(),
-            contentPadding = PaddingValues(32.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp)
+            TvLazyColumn(
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = PaddingValues(32.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                item {
+                    Text(
+                        text = stringResource(id = state.appSettings.first),
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurface,
+                    )
+                }
+                items(state.appSettings.second) {
+                    SettingsItem(item = it, selectedItem = selectedItem, updateSetting = updateSetting)
+                }
+
+                item {
+                    Text(
+                        text = stringResource(id = state.personalSettings.first),
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurface,
+                    )
+                }
+                items(state.personalSettings.second) {
+                    SettingsItem(item = it, selectedItem = selectedItem, updateSetting = updateSetting)
+                }
+            }
+        }
+
+        AnimatedVisibility(
+            visible = selectedItem.value != null,
+            enter = slideInHorizontally(initialOffsetX = { it }, animationSpec = tween(300)),
+            exit = slideOutHorizontally(targetOffsetX = { it }, animationSpec = tween(300)),
+            modifier = Modifier.weight(1f)
         ) {
-            item {
-                Text(
-                    text = stringResource(id = state.appSettings.first),
-                    style = MaterialTheme.typography.bodySmall,
-                    color = MaterialTheme.colorScheme.onSurface,
-                )
-            }
-            items(state.appSettings.second) {
-                SettingsItem(item = it)
-            }
-
-            item {
-                Text(
-                    text = stringResource(id = state.personalSettings.first),
-                    style = MaterialTheme.typography.bodySmall,
-                    color = MaterialTheme.colorScheme.onSurface,
-                )
-            }
-            items(state.personalSettings.second) {
-                SettingsItem(item = it)
+            selectedItem.value?.let {
+                SettingsDetail(item = selectedItem.value!!, updateSetting = updateSetting)
             }
         }
     }
@@ -91,11 +122,16 @@ private fun SettingsContent(
 @OptIn(ExperimentalTvMaterial3Api::class)
 @Composable
 private fun SettingsItem(
-    item: SettingsItemUIState
+    item: SettingsItemUIState,
+    selectedItem: MutableState<SettingsItemUIState?>,
+    updateSetting: (SettingsItemUIState) -> Unit
 ) {
     ListItem(
         selected = false,
-        onClick = {},
+        onClick = {
+            selectedItem.value = item
+            updateSetting(item)
+                  },
         leadingContent = {
             Text(
                 text = item.title,
@@ -128,9 +164,77 @@ private fun SettingsItem(
     )
 }
 
-val settingsItemsTitle = listOf(
-    "Units preference",
-    "Language",
-    "Video Resolution",
-    "Subtitles",
-)
+
+@OptIn(ExperimentalTvMaterial3Api::class)
+@Composable
+private fun SubSettingItem(
+    item: SettingsItemUIState,
+    selectedItem: MutableState<SettingsItemUIState?>,
+    updateSetting: (SettingsItemUIState) -> Unit
+) {
+    item.possibleValues.forEach { subSetting ->
+        ListItem(
+            onClick = {
+                val updatedItem = item.copy(value = subSetting)
+                updateSetting(updatedItem)
+                selectedItem.value = updatedItem
+            },
+            selected = selectedItem.value?.value == subSetting,
+            leadingContent = {
+                Text(
+                    text = subSetting,
+                    style = MaterialTheme.typography.titleMedium,
+                )
+            },
+            trailingContent = {
+                if (selectedItem.value?.value == subSetting) {
+                    Icon(
+                        Icons.Default.CheckCircle,
+                        modifier = Modifier.size(ListItemDefaults.IconSize),
+                        contentDescription = "selected icon"
+                    )
+                }
+            },
+            headlineContent = {},
+            colors = ListItemDefaults.colors(
+                containerColor = surface,
+                contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                focusedContentColor = MaterialTheme.colorScheme.surfaceVariant,
+                selectedContainerColor = surface
+            ),
+            modifier = Modifier.padding(4.dp)
+        )
+    }
+}
+@OptIn(ExperimentalTvMaterial3Api::class)
+@Composable
+private fun SettingsDetail(
+    item: SettingsItemUIState,
+    updateSetting: (SettingsItemUIState) -> Unit
+) {
+    Column(
+        Modifier
+            .background(Color(0xFF0B0F0E))
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = item.title,
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.padding(top = 64.dp, start = 32.dp)
+        )
+        TvLazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(32.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(listOf(item)) {
+                SubSettingItem(
+                    updateSetting = updateSetting,
+                    item = item,
+                    selectedItem = remember { mutableStateOf(item) })
+            }
+        }
+    }
+}
--- JetFit/app/src/main/java/com/google/jetfit/presentation/screens/settings/SettingsViewModel.kt
+++ JetFit/app/src/main/java/com/google/jetfit/presentation/screens/settings/SettingsViewModel.kt
@@ -15,5 +15,19 @@ class SettingsViewModel @Inject constructor(
     private val _settingsUIState = MutableStateFlow(SettingsUIState())
     val settingsUIState = _settingsUIState.asStateFlow()
 
+    fun updateSetting(item: SettingsItemUIState) {
+        val appSetting = SettingsMenuUIState.AppSettings.entries.find { it.name == item.title }
+        val personalSetting = SettingsMenuUIState.PersonalSettings.entries.find { it.name == item.title }
 
+        appSetting?.value = item.value
+        personalSetting?.value = item.value
+
+        val updatedAppSettings = _settingsUIState.value.appSettings.second.map { if (it.title == item.title) item else it }
+        val updatedPersonalSettings = _settingsUIState.value.personalSettings.second.map { if (it.title == item.title) item else it }
+
+        _settingsUIState.value = _settingsUIState.value.copy(
+            appSettings = _settingsUIState.value.appSettings.first to updatedAppSettings,
+            personalSettings = _settingsUIState.value.personalSettings.first to updatedPersonalSettings
+        )
+    }
 }
